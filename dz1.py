bd6777c8ddc0faf5c1ca9994ab35f504461ab48c
Natallia Bela <nataliagerceg@icloud.com>
2022-09-25 11:04:19 -0700

    Initial commit

---
+++ dz1.py
@@ -0,0 +1,26 @@
+a = 546
+print(a)
+
+
+b = 'Моя первая программа'
+print(b)
+
+
+c = [546, 'Моя первая программа']
+print(c)
+
+
+d = {
+    1: 'orange',
+    2: 'mango',
+}
+print(d)
+
+
+w = 234
+q = a * w
+print(q)
+
+
+e = a + q
+print(e)
